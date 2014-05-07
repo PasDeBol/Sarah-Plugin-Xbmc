7db433ae36e9d1c88e60484868887a7939496921
PasDeBol <sgt.pasdebol@gmail.com>
2014-05-07 18:41:13 +0200

    Amélioration test de configuration

--- plugins/xbmctools.js
+++ plugins/xbmctools.js
@@ -1,72 +1,80 @@
 module.exports = {
 	// 	Test configuration et connection
 	testconfig: function (config,callback) {
-		htmlmusic= '####################<br>';
-		htmlmusic+='# Compte rendu du test #<br>';
-		htmlmusic+='####################<br>';
+		htmlreponse= '####################<br>';
+		htmlreponse+='# Compte rendu du test #<br>';
+		htmlreponse+='####################<br>';
 		// charge les url de config
 		configurlmusic=config.api_url_xbmc_music;
 		configurlvideo=config.api_url_xbmc_video;
 		warning=false;
 		// test la structure de l'url
-		htmlmusic+='<br>Contrôle des paramètres votre configuration:';
+		htmlreponse+='<br>Contrôle des paramètres votre configuration:';
 
 		regexp = RegExp('.*:.*', 'gi');
 		if (regexp.test(configurlmusic)!=true) {
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: api_url_xbmc_music n\'a pas un format valide. <--- Configurez le plugin correctement';
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port';
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX votre config  : '+configurlmusic ;
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: api_url_xbmc_music n\'a pas un format valide. <--- Configurez le plugin correctement';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config  : '+configurlmusic ;
+			callback({'tts': htmlreponse});
 			return;
 			}
 		regexp = RegExp('.*:.*', 'gi');
 		if (regexp.test(configurlvideo)!=true) {
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: api_url_xbmc_video n\'a pas un format valide. <--- Configurez le plugin correctement';
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port';
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlmusic ;
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: api_url_xbmc_video n\'a pas un format valide. <--- Configurez le plugin correctement';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlvideo ;
+			callback({'tts': htmlreponse});
 			return;
 			}
-		htmlmusic+='</br>&nbsp;&nbsp;&nbsp;|| OK';
+		htmlreponse+='</br>&nbsp;&nbsp;&nbsp;|| OK';
 		// test le format de l'IP et du port
 		regexp = RegExp(/^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){2}(\.([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])):/);
-		htmlmusic+='</br>Analyse des IP:';
+		htmlreponse+='</br>Analyse des IP:';
 		if (regexp.test(configurlmusic)!=true) {
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR : la configuration de api_url_xbmc_music ('+configurlmusic+') ne contient pas d\'ip valable! <--- Configurez le plugin correctement';
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR : la configuration de api_url_xbmc_music ('+configurlmusic+') ne contient pas d\'ip valable! <--- Configurez le plugin correctement';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port (sans "http://") ';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlmusic ;
+			callback({'tts': htmlreponse});
 			return;
 		}
 		regexp = RegExp(/^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){2}(\.([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])):/);
 		if (regexp.test(configurlvideo)!=true) {
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: la configuration de api_url_xbmc_video ('+configurlvideo+') ne contient pas d\'ip valable! <--- Configurez le plugin correctement';
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX ERREUR: la configuration de api_url_xbmc_video ('+configurlvideo+') ne contient pas d\'ip valable! <--- Configurez le plugin correctement';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port (sans "http://") ';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlvideo ;
+			callback({'tts': htmlreponse});
 			return;
 		}
 		regexp = RegExp(/:([0-9]){1,5}$/);
 		if (regexp.test(configurlmusic)!=true) {
-			htmlmusic+='</br>ERREUR: la configuration de api_url_xbmc_music ('+configurlmusic+') ne contient pas de port valable! <--- Configurez le plugin correctement)';
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>ERREUR: la configuration de api_url_xbmc_music ('+configurlmusic+') ne contient pas de port valable! <--- Configurez le plugin correctement)';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port (sans "/jsonrpc") ';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlmusic ;
+			callback({'tts': htmlreponse});
 			return;
 		}
 		regexp = RegExp(/:([0-9]){1,5}$/);
 		if (regexp.test(configurlvideo)!=true) {
-			htmlmusic+='</br>ERREUR: la configuration de api_url_xbmc_video ('+configurlvideo+') ne contient pas de port valable! <--- Configurez le plugin correctement)';
-			callback({'tts': htmlmusic});
+			htmlreponse+='</br>ERREUR: la configuration de api_url_xbmc_video ('+configurlvideo+') ne contient pas de port valable! <--- Configurez le plugin correctement)';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX attendu: IP:port (sans "/jsonrpc") ';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX votre config: '+configurlvideo ;
+			callback({'tts': htmlreponse});
 			return;
 		}
-		htmlmusic+='</br>&nbsp;&nbsp;&nbsp;|| OK';
+		htmlreponse+='</br>&nbsp;&nbsp;&nbsp;|| OK';
 		var sys = require('sys')
 		var exec = require('child_process').exec;
 		function puts(error, stdout, stderr) {
 			console.log('111');
 			regexp = RegExp('perdus = 1', 'gi');
-			htmlmusic+="</br>Ping de l\'ip xbmc 'music' :";
+			htmlreponse+="</br>Ping de l\'ip xbmc 'music' :";
 			if (regexp.test(stdout)==true) {
-				htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Erreur : '+ip+' ne réponds pas au ping <--- bonne IP??';
-				callback({'tts': htmlmusic});
+				htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Erreur : '+ip+' ne réponds pas au ping <--- bonne IP??';
+				callback({'tts': htmlreponse});
 				return;
 			}
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;|| OK';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;|| OK';
 			regexp = RegExp('.*:', 'gi');
 			result=configurlvideo.match(regexp);
 			ip=result[0].toString().slice(0,result[0].length-1);
@@ -76,27 +84,27 @@ module.exports = {
 		}
 		function puts2(error, stdout, stderr) {
 			regexp = RegExp('perdus = 1', 'gi');
-			htmlmusic+="</br>Ping de l\'ip xbmc 'video' :";
+			htmlreponse+="</br>Ping de l\'ip xbmc 'video' :";
 			if (regexp.test(stdout)==true) {
-				htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Erreur : '+ip+' ne réponds pas au ping <--- bonne IP??';
-				callback({'tts': htmlmusic});
+				htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Erreur : '+ip+' ne réponds pas au ping <--- bonne IP??';
+				callback({'tts': htmlreponse});
 				return;
 			}
-			htmlmusic+='</br>&nbsp;&nbsp;&nbsp;|| OK';
+			htmlreponse+='</br>&nbsp;&nbsp;&nbsp;|| OK';
 			go_port_music();
 		}
 		function go_port_music() {
-			htmlmusic+='</br>Test le port de destination xbmc "music":';
+			htmlreponse+='</br>Test le port de destination xbmc "music":';
 			var request = require('request');
 			request({'uri': 'http://'+configurlmusic,'method': 'GET', 'timeout': 2000}, function (err, response) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Erreur: Aucune réponse sur le port '+configurlmusic+' <--- Port Correct?';
-					htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Le port doit être celui que vous avez renseigné dans XBMC (serveur WEB)';
-					callback({'tts': htmlmusic});
+					htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Erreur: Aucune réponse sur le port '+configurlmusic+' <--- Port Correct?';
+					htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Le port doit être celui que vous avez renseigné dans XBMC (serveur WEB)';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| OK';
-				htmlmusic+='</br>Analyse du serveur web XBMC sur le port xbmc "music":';
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| OK';
+				htmlreponse+='</br>Analyse du serveur web XBMC sur le port xbmc "music":';
 				regexp = RegExp('<title>.*</title>', 'gi');
 				if (regexp.test(response.body)==true) {
 					result=response.body.match(regexp);
@@ -107,89 +115,89 @@ module.exports = {
 				}
 				
 				if (site!='XBMC') {
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: Ce n\'est pas XBMC qui réponds sur ce port! <--- Vous vous trompez de PC donc d\'IP ou alors ce n\'est le bon port?';
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Info :  C\'est "'+site+ '" qui réponds sur ce port! ';
-					callback({'tts': htmlmusic});
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: Ce n\'est pas XBMC qui réponds sur ce port! <--- Vous vous trompez de PC donc d\'IP ou alors ce n\'est le bon port?';
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Info :  C\'est "'+site+ '" qui réponds sur ce port! ';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+				htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
 				go_port_video();
 			});
 		}
 		function go_port_video() {
-			htmlmusic+='</br>Test le port de destination xbmc "video":';
+			htmlreponse+='</br>Test le port de destination xbmc "video":';
 			var request = require('request');
 			request({'uri': 'http://'+configurlvideo,'method': 'GET', 'timeout': 2000}, function (err, response) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Erreur: Aucune réponse sur le port '+configurlvideo+' <--- Port Correct?';
-					htmlmusic+='</br>&nbsp;&nbsp;&nbsp;XX Le port doit être celui que vous avez renseigné dans XBMC (serveur WEB)';
-					callback({'tts': htmlmusic});
+					htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Erreur: Aucune réponse sur le port '+configurlvideo+' <--- Port Correct?';
+					htmlreponse+='</br>&nbsp;&nbsp;&nbsp;XX Le port doit être celui que vous avez renseigné dans XBMC (serveur WEB)';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| OK';
-				htmlmusic+='</br>Analyse du serveur web XBMC sur le port de xbmc "video":';
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| OK';
+				htmlreponse+='</br>Analyse du serveur web XBMC sur le port de xbmc "video":';
 				regexp = RegExp('<title>.*</title>', 'gi');
 				result=response.body.match(regexp);
 				site=result[0].slice(7,result[0].length-8);
 				if (site!='XBMC') {
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: Ce n\'est pas XBMC qui réponds sur ce port! <--- Vous vous trompez de PC donc d\'IP ou alors ce n\'est le bon port?';
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Info :  C\'est "'+site+ '" qui réponds sur ce port! ';
-					callback({'tts': htmlmusic});
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: Ce n\'est pas XBMC qui réponds sur ce port! <--- Vous vous trompez de PC donc d\'IP ou alors ce n\'est le bon port?';
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Info :  C\'est "'+site+ '" qui réponds sur ce port! ';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+				htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
 				go_port_xbmc_music();
 			});
 		}
 		function go_port_xbmc_music() {
-			htmlmusic+='</br>Test la réponse de xbmc "music":';
+			htmlreponse+='</br>Test la réponse de xbmc "music":';
 			var request = require('request');
 			var rqjson={"jsonrpc": "2.0", "method": "GUI.GetProperties", "params": { "properties": ["currentwindow"]}, "id": 1};
 			request({'uri': 'http://'+configurlmusic+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000}, function (err, response, json) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! Version de xbmc?';
-					callback({'tts': htmlmusic});
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! Version de xbmc?';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| OK';
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| Info: votre XBMC "music" est actuellement sur la fenetre :'+json.result.currentwindow.label;
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| OK';
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| Info: votre XBMC "music" est actuellement sur la fenetre :'+json.result.currentwindow.label;
 				go_port_xbmc_video();
 			});
 		}
 		function go_port_xbmc_video() {
-			htmlmusic+='</br>Test la réponse de xbmc "video":';
+			htmlreponse+='</br>Test la réponse de xbmc "video":';
 			var request = require('request');
 			var rqjson={"jsonrpc": "2.0", "method": "GUI.GetProperties", "params": { "properties": ["currentwindow"]}, "id": 1};
 			request({'uri': 'http://'+configurlvideo+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000}, function (err, response, json) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! Version de xbmc?';
-					callback({'tts': htmlmusic});
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! Version de xbmc?';
+					callback({'tts': htmlreponse});
 					return;
 				}
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| OK';
-				htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| Info: votre XBMC "video" est actuellement sur la fenetre :'+json.result.currentwindow.label;
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| OK';
+				htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| Info: votre XBMC "video" est actuellement sur la fenetre :'+json.result.currentwindow.label;
 				goto_mediathequemusic();
 			});
 		}
 		
 		function goto_mediathequemusic() {
-			htmlmusic+='</br>Test de la médiathèque de xbmc "music":';
+			htmlreponse+='</br>Test de la médiathèque de xbmc "music":';
 			var request = require('request');
 			var rqjson={"jsonrpc": "2.0", "method": "AudioLibrary.GetArtists", "params": {}, "id": 1};
 			request({'uri': 'http://'+configurlmusic+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000}, function (err, response, json) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! pas de médiathèque musicale dans XBMC?";
-					callback({'tts': htmlmusic});
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! pas de médiathèque musicale dans XBMC?";
+					callback({'tts': htmlreponse});
 					return;
 				}
 				if (json.result.limits.total==0) {
 					warning=true;
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
-					htmlmusic+="<br>|| WARNING -Aucun  artiste <--- Il faut ajouter vos titre à la médiathèque de XBMC";
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+					htmlreponse+="<br>|| WARNING -Aucun  artiste <--- Il faut ajouter vos titre à la médiathèque de XBMC";
 					}
 				else {
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| Info:  Nombre d\'artistes :'+json.result.limits.total;
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| Info:  Nombre d\'artistes :'+json.result.limits.total;
 				}
 				rqjson={"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Félicitation!","message":"Sarah communique moi!"}};
 				if (configurlvideo!=configurlmusic) request({'uri': 'http://'+configurlmusic+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000});
@@ -198,51 +206,51 @@ module.exports = {
 		}
 
 		function goto_mediathequevideo() {
-			htmlmusic+='</br>Test de la médiathèque de xbmc "video":';
+			htmlreponse+='</br>Test de la médiathèque de xbmc "video":';
 			var request = require('request');
 			var rqjson={"jsonrpc": "2.0", "method": "VideoLibrary.GetMovies", "params": {}, "id": 1};
 			request({'uri': 'http://'+configurlvideo+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000}, function (err, response, json) {
 				if (err || response.statusCode != 200) {
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! pas de film mis en médiathèque?";
-					callback({'tts': htmlmusic});
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;XX Erreur: réponse incorrecte de XBMC <--- Etrange! pas de film mis en médiathèque?";
+					callback({'tts': htmlreponse});
 					return;
 				}
 				if (json.result.limits.total==0) {
 					warning=true;
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
-					htmlmusic+="<br>|| WARNING -Aucun  film <--- Il faut ajouter vos titre à la médiathèque de XBMC";
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+					htmlreponse+="<br>|| WARNING -Aucun  film <--- Il faut ajouter vos titre à la médiathèque de XBMC";
 					}
 				else {
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
-					htmlmusic+='<br>&nbsp;&nbsp;&nbsp;|| Info:  Nombre de films :'+json.result.limits.total;
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+					htmlreponse+='<br>&nbsp;&nbsp;&nbsp;|| Info:  Nombre de films :'+json.result.limits.total;
 				}
 				goto_controletheme();
 			});
 		}
 		function goto_controletheme() {
-			htmlmusic+='</br>Controle du theme/skin xbmc "video":';
+			htmlreponse+='</br>Controle du theme/skin xbmc "video":';
 			var request = require('request');
 			var rqjson={"jsonrpc": "2.0", "method": "GUI.GetProperties", "params": { "properties": ["skin"]}, "id": 1};
 			request({'uri': 'http://'+configurlvideo+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000}, function (err, response, json) {
 				if ((json.result.skin.name!='Confluence')&&(json.result.skin.name!='Aeon Nox')) {
 					warning=true;
-					htmlmusic+="<br>|| WARNING votre thème est "+json.result.skin.name+", ce n'est pas le thème par défaut. Cela peut éventuellement poser problème en 'mode xbmc'";
-					htmlmusic+="<br>|| Cela peut éventuellement poser problème en 'mode xbmc'";
+					htmlreponse+="<br>|| WARNING votre thème est "+json.result.skin.name+", ce n'est pas le thème par défaut. Cela peut éventuellement poser problème en 'mode xbmc'";
+					htmlreponse+="<br>|| Cela peut éventuellement poser problème en 'mode xbmc'";
 					
 					}
 				else {
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| OK";
-					htmlmusic+="<br>&nbsp;&nbsp;&nbsp;|| Info: votre thème est "+json.result.skin.name;
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| OK";
+					htmlreponse+="<br>&nbsp;&nbsp;&nbsp;|| Info: votre thème est "+json.result.skin.name;
 				}
 				
-				htmlmusic+='</br><br>';
-				htmlmusic+='################<br>';
-				htmlmusic+='# Tout est correct! #<br>';
-				if (warning) htmlmusic+='# Mais Warning! #<br>';
-				htmlmusic+='################<br>';
+				htmlreponse+='</br><br>';
+				htmlreponse+='################<br>';
+				htmlreponse+='# Tout est correct! #<br>';
+				if (warning) htmlreponse+='# Mais Warning! #<br>';
+				htmlreponse+='################<br>';
 				rqjson={"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Félicitation!","message":"Sarah communique moi!"}};
 				request({'uri': 'http://'+configurlvideo+'/jsonrpc','method': 'POST', 'json' : rqjson , 'timeout': 2000});
-				callback({'tts': htmlmusic});
+				callback({'tts': htmlreponse});
 		
 			});
 		}
